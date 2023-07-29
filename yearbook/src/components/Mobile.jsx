3cf221ef83736f927793db1135cad0246d2c516b
therealbeez <gabriel.agbese2@gmail.com>
2023-07-29 09:09:21 +0200

    speech modal first pass

--- yearbook/src/components/Mobile.jsx
+++ yearbook/src/components/Mobile.jsx
@@ -59,7 +59,7 @@ function Mobile() {
 				<div className='mobile-hero'>
 					<div className='hero-top'>Lightway Academy Senior Class Of 2023</div>
 					<div className='hero-image'>
-						
+
 					</div>
 				</div>
 				<div className='welcome-space'>Welcome.</div>
@@ -80,7 +80,24 @@ function Mobile() {
 							Graduation marks the end of one journey and the beginning of another. As you step out into the world beyond these walls, I want to share a few thoughts with you.</p>
 						<button onClick={openDirectorModal} className='read-more'>Read more</button>
 						<dialog ref={directorModalRef} className='director-modal'>
-							<p onClick={closeDirectorModal}>close</p>
+							<p onClick={closeDirectorModal} className='close'>Close</p>
+							<p>Dear graduates,
+
+								As the school Director, it brings me immense joy and pride to stand before you today as you embark on this exciting new chapter of your lives. Congratulations to each and every one of you for reaching this significant milestone.
+
+								Graduation marks the end of one journey and the beginning of another. As you step out into the world beyond these walls, I want to share a few thoughts with you.
+
+								First and foremost, remember that success is not measured by comparing yourself to others. I know you’ve heard it before but let in sink in afresh that each of you is unique, with your own set of talents and passions. Embrace your individuality and remember that your path in life will be different from your peers. It's essential to focus on your personal growth and progress rather than getting lost in comparison.
+
+								Tony Elumelu recently said, "I often tell my children that winning starts in their minds, rather than at the finish line. It begins each day till you achieve your goal. However, you must have self-discipline and robust time management skills to win. “ Success is never accidental; it is the result of our deliberate efforts to achieve greatness. This quote reminds us that achievements come from dedication, hard work, and perseverance. No matter where you go or what you do, always give your best, and success will follow.
+
+								Life is an unpredictable journey filled with challenges and opportunities. Embrace both, for they are the building blocks of growth. Embrace failure as a stepping stone to learning and improvement. Embrace success as a testament to your efforts and dedication.
+
+								As you leave this institution and venture into the world, remember that learning doesn't end here. Be lifelong learners, continuously seeking knowledge, new skills, and fresh perspectives. Change is inevitable so embrace it and be adaptable, for it will open doors to unforeseen possibilities.
+
+								We thank you dear parents for allowing us the privilege to help shape your children into great men and women. Congratulations once again graduating learners; may your future be filled with boundless success and happiness. We pray that the grace of the Almighty God will continue to keep us all.
+
+								Thank you.</p>
 						</dialog>
 					</div>
 
@@ -119,7 +136,14 @@ function Mobile() {
 						<p className='speech-external'>Warm greetings to all, I would like to use this opportunity to appreciate and thank my God for without him I would not be here today, the Director, the Principal Vice Principals and the school management for all my years till this very moment in this prestigious and amazing school.</p>
 						<button onClick={openHeadboyModal} className='read-more'>read more</button>
 						<dialog ref={headboyModalRef} className='headboy-modal'>
-							<p onClick={closeHeadboyModal}>close</p>
+							<p onClick={closeHeadboyModal} className='close'>Close</p>
+							<p>Warm greetings to all, I would like to use this opportunity to appreciate and thank my God for without him I would not be here today, the Director, the Principal Vice Principals and the school management for all my years till this very moment in this prestigious and amazing school.
+
+Okay, let's get into it. I would like to say that this is the beginning to a next and new level of life. The things we did before and the things we do now are going to influence what we do in the future, I would like to quote myself on that. I came to this school in the year 2012 as a primary 2 pupil, that is about eleven years in this school, and ever since I can remember I was always told be my parents and teachers to take my studies seriously. Being a little child of zero experience then, I always thought they bothered me a lot and I always wanted to just play.
+
+Everything about this school, most especially the teachers has shaped me as a person being here for most of my life in a positive and strong way. To my friends, I am grateful for your support. Soluchukwu my rival and best friend who pushed me to go far in my academics, Harry who is like a brother to me, Jedidiah the unchanging who always cares no matter what and who'd always give me a hug, Gaza my rival the most encouraging friend who pushed me to go farther in sports with his strong character and personality, the beautiful Tehilah, my coworker who is hardworking, Flourish who always understood me, Clinton, Chigbo and Oty who made my days in Lightway fun and full of laughter, Lauraine who is work-shy but exceptionally smart and intelligent, Duchess, the nice and friendly one, Pearl the quiet and cheerful one, the lovely Elizabeth who I grew up with since Js1, Laila, Precious, Daniella and Nneoma who are always kind to me, who are one of a kind.
+
+I have always entered the school gate with a feeling of being cared for as if I were with family and I will cherish all those memories and experiences. Today I will leave the school gate in finality but I will leave as a grown man who has left his family to become someone better and a success. To the Director and School Management, it truly is an honour to finish as the head boy of this school! Farewell! Sayonara!</p>
 						</dialog>
 					</div>
 
@@ -199,7 +223,7 @@ function Mobile() {
 					<div className='mobile-student thomas'>
 						<p className='mobile-student-name'>Elizabeth Abrakassa THOMAS</p>
 					</div>
-					
+
 					<div className='mobile-student clinton'>
 						<p className='mobile-student-name'>Clinton Chinonso UWAKWE</p>
 					</div>
@@ -212,7 +236,7 @@ function Mobile() {
 					<p className='mobile-rec-title'>Achievements and Recognition</p>
 					<div className='rec-writeup'>
 						<p className='rec-writeup-content'>Throughout the academic year, our school has been a melting pot of brilliance, determination, and dedication. From academic excellence to artistic brilliance, from sportsmanship to community service, our students have left an indelible mark, proving that they are the true embodiment of our school's values and spirit.
-Some highlights include:</p>
+							Some highlights include:</p>
 					</div>
 					<div className='mobile-rec-menu'>
 
